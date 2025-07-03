8c2ff33fcadb02aed34b96ea14bf296d6ab79b21
ruanbw <1813967922@qq.com>
2025-07-03 12:01:45 +0800

    feat: transition component animation

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <router-view />
+  <transition name="slide-fade" mode="out-in">
+    <router-view>
+      <template #default="{ Component }">
+        <Component :is="Component"/>
+      </template>
+    </router-view>
+  </transition>
 </template>
