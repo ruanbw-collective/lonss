5a7ca2d3f2a58a2a708cfca917dc3dc70b2ff206
ruanbw <1813967922@qq.com>
2025-08-05 03:55:49 +0800

    chore: setup project monorepo mode

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <transition name="slide-fade" mode="out-in">
-    <router-view>
-      <template #default="{ Component }">
-        <Component :is="Component"/>
-      </template>
-    </router-view>
-  </transition>
-</template>
